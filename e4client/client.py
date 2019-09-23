2b3730816a2846fafdb0816a81b6ff460054b093
Manuel Olguin <manuel@olguin.se>
2019-09-23 15:08:00 -0400

    added code for shutting down client and having it be a context manager

--- e4client/client.py
+++ e4client/client.py
@@ -24,13 +24,13 @@ class ServerRequestError(Exception):
     pass
 
 
-class E4StreamingClient(threading.Thread):
+class E4StreamingClient(AbstractContextManager):
     _delim = b'\n'
 
     def __init__(self, server_ip: str,
                  server_port: int,
                  max_conn_attempts: int = 20):
-        super().__init__()
+        super().__init__(self)
 
         self._logger = logging.getLogger(self.__class__.__name__)
 
@@ -65,44 +65,58 @@ class E4StreamingClient(threading.Thread):
                     self._socket.close()
                     raise
 
+        self._connected = True
+
         self._recv_thread = threading.Thread(target=self._recv_loop,
                                              daemon=True)
         self._recv_thread.start()
+
+    @property
+    def is_connected(self) -> bool:
+        return self._connected
 
     def _recv_loop(self):
         self._logger.debug('Starting receiving thread...')
 
         data = b''
         while True:
-            # small block size since messages are short
-            data += self._socket.recv(64)
+            try:
+                # small block size since messages are short
+                data += self._socket.recv(64)
 
-            # split up responses and process them
-            while True:
-                raw_msg, lim, rest = data.partition(E4StreamingClient._delim)
-                if len(lim) == len(rest) == 0:
-                    # no remaining complete messages, read again from socket
-                    break
+                # split up responses and process them
+                while True:
+                    raw_msg, lim, rest = \
+                        data.partition(E4StreamingClient._delim)
+                    if len(lim) == len(rest) == 0:
+                        # no remaining complete messages, read again from socket
+                        break
 
-                data = rest  # save the remaining data for further processing
+                    data = rest  # save the remaining data for further
+                    # processing
 
-                # parse the first extracted response
-                message = raw_msg.decode('utf-8')
-                self._logger.debug(f'Raw incoming message: {message}')
+                    # parse the first extracted response
+                    message = raw_msg.decode('utf-8')
+                    self._logger.debug(f'Raw incoming message: {message}')
 
-                msg_type, parsed_msg = _parse_incoming_message(message)
-                self._logger.debug(f'Parsed message: {parsed_msg}')
+                    msg_type, parsed_msg = _parse_incoming_message(message)
+                    self._logger.debug(f'Parsed message: {parsed_msg}')
 
-                if msg_type == _ServerMessageType.STREAM_DATA:
-                    self.sub_qs[parsed_msg.stream].put(
-                        (parsed_msg.timestamp, *parsed_msg.data))
-                else:
-                    while True:
-                        try:
-                            self._resp_q.get_nowait()
-                        except queue.Empty:
-                            self._resp_q.put_nowait(parsed_msg)
-                            break
+                    if msg_type == _ServerMessageType.STREAM_DATA:
+                        self.sub_qs[parsed_msg.stream].put(
+                            (parsed_msg.timestamp, *parsed_msg.data))
+                    else:
+                        while True:
+                            try:
+                                self._resp_q.get_nowait()
+                            except queue.Empty:
+                                self._resp_q.put_nowait(parsed_msg)
+                                break
+
+            except socket.error as e:
+                self._logger.debug(e)
+                self._logger.debug('Shutting down receiving thread...')
+                return
 
     def _send(self, cmd: str):
         self._logger.debug(f'Sending \'{cmd.encode("utf-8")}\' to server.')
@@ -115,6 +129,9 @@ class E4StreamingClient(threading.Thread):
 
         assert resp.command == cmd_id
         return resp
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
 
     # public convenience methods follow:
 
@@ -151,6 +168,13 @@ class E4StreamingClient(threading.Thread):
 
         if resp.status != _CmdStatus.SUCCESS:
             raise ServerRequestError(resp.data)
+
+    def close(self):
+        if self._connected:
+            self._socket.shutdown(socket.SHUT_RDWR)
+            self._socket.close()
+            self._recv_thread.join()
+            self._connected = False
 
 
 class DeviceConnection(AbstractContextManager):
