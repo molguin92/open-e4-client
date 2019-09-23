820116b9d1f939997e9ce248452f2a64d4503366
Manuel Olguin <manuel@olguin.se>
2019-09-23 18:42:04 -0400

    preparing pypi release

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import setuptools
 
+pkg_name = 'open-e4-client'
+version = '0.0.3beta1'
+
 setuptools.setup(
-    name="open-e4-client",
-    version="0.0.3beta1",
+    name=pkg_name,
+    version=version,
     author="Manuel Olguin Munoz",
     author_email="molguin@kth.se",
     description="Simple, pure Python 3.7 client for the "
                 "Empatica E4 streaming server.",
     long_description_content_type="text/markdown",
     url="https://github.com/molguin92/open-e4-client",
+    download_url=f'https://github.com/molguin92/'
+                 f'{pkg_name}/archive/{version}.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
